ed8dc0ee79b64b94214e15650342e48268411a35
Nic <nppasqua@buffalo.edu>
2018-11-03 16:03:22 -0400

    template set

--- myflaskapp.py
+++ myflaskapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +6,4 @@ def render():
     return render_template('index.html')
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run()
